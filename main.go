85be30a6bcf783e171be402d7dcf8704cf5af927
naitwo2 <naito@ap-com.co.jp>
2020-09-13 07:13:20 +0000

    End of work today.

--- main.go
+++ main.go
@@ -1,34 +1,64 @@
 package main
 
 import (
-    "encoding/csv"
+    "bufio"
     "fmt"
     "io"
-    "os"    
+	"os"
+	"strings"
+
 )
 
-func userCsvNewReader(fileName string) {
-    fp, err := os.Open(fileName)
-    if err != nil {
-        panic(err)
-    }
-    defer fp.Close()
-
-    reader := csv.NewReader(fp)
-    reader.Comma = ','
-    reader.LazyQuotes = true
-    for {
-        record, err := reader.Read()
-        if err == io.EOF {
-            break
-        } else if err != nil {
-            panic(err)
-        }
-        fmt.Println(record)
+func main() {
+    if err := readLine("pa_2行.log"); err != nil {
+        fmt.Println(os.Stderr, err)
+        os.Exit(1)
     }
 }
 
+func readLine(filename string) error {
+	logdataMap := map[string]string{}
 
-func main(){
-    userCsvNewReader("pa_2行.log")
+    file, err := os.Open(filename)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    reader := bufio.NewReader(file)
+
+    for {
+        line, isPrefix, err := reader.ReadLine()
+        if err == io.EOF {
+            break
+        }
+        if err != nil {
+            return err
+        }
+		//fmt.Print(string(line))
+		
+		// カンマでスライス
+		slice := strings.Split(string(line), ",")
+
+/* 		for i := 0; i < len(slice); i +=2 {
+			logdataMap[i] = slice[i+1]
+		} */
+
+		i := 0
+  		for _, str := range slice {
+			logdataMap["hoge"] = str
+			i = i + 1
+		}  
+
+		fmt.Println(logdataMap)
+		//fmt.Printf(logdataMap[])
+
+        if !isPrefix {
+            fmt.Println()
+        }
+	}
+
+
+
+    return nil
 }
